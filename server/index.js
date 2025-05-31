3b836039acd82a262f3fdaebea29c7288421d81d
vijaybhagat15 <vijaybhagatmitlab@gmail.com>
2025-05-31 10:32:42 +0530

    updated all files

--- server/index.js
+++ server/index.js
@@ -93,60 +93,6 @@ process.on("unhandledRejection", (reason, promise) => {
   logger.error("Unhandled Promise Rejection:", { reason, promise });
 });
 
-// app.post('/forgot-password', (req, res) => {
-//     const {email} = req.body;
-//     AuthUserModel.findOne({email: email})
-//     .then(user => {
-//         if(!user) {
-//             return res.send({Status: "User not existed"})
-//         } 
-//         const token = jwt.sign({id: user._id}, process.env.SUPERADMIN_JWT_SECRET, {expiresIn: "1d"})
-//         var transporter = nodemailer.createTransport({
-//             service: 'gmail',
-//             auth: {
-//                 user: process.env.EMAIL_USER,
-//                 pass: process.env.EMAIL_PASS,
-//             }
-//           });
-          
-//           var mailOptions = {
-//             from: process.env.EMAIL_USER,
-//             to: email,
-//             subject: 'Reset Password Link',
-//             text: `http://localhost:5173/reset_password/${user._id}/${token}`
-//           };
-          
-//           transporter.sendMail(mailOptions, function(error, info){
-//             if (error) {
-//               console.log(error);
-//               return res.status(500).json({ Status: "Email failed", error });
-
-//             } else {
-//               return res.send({Status: "Success"})
-//             }
-//           });
-//     })
-// })
-
-// app.post('/reset-password/:id/:token', (req, res) => {
-//     const {id, token} = req.params
-//     const {password} = req.body
-
-//     jwt.verify(token, process.env.SUPERADMIN_JWT_SECRET, (err, decoded) => {
-//         if(err) {
-//             return res.json({Status: "Error with token"})
-//         } else {
-//             bcrypt.hash(password, 10)
-//             .then(hash => {
-//                 AuthUserModel.findByIdAndUpdate({_id: id}, {password: hash})
-//                 .then(u => res.send({Status: "Success"}))
-//                 .catch(err => res.send({Status: err}))
-//             })
-//             .catch(err => res.send({Status: err}))
-//         }
-//     })
-// })
-
 // default route
 app.use("/*", (req, res) => {
   return res.status(404).json({
